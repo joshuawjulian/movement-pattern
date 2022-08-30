f5dbeb736fdfdc1f2b06524dd1dd88877d747131
Joshua W Julian <joshuawjulian@gmail.com>
2022-08-30 08:20:56 -0400

    Vercel Revert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,26 @@
 	import '../app.css';
 </script>
 
-<div class="w-full h-full">
-	<main class="flex flex-col w-full h-full">
+<div class="w-full h-full grid grid-cols-1 lg:grid-cols-6">
+	<aside class="hidden lg:block col-start-1 col-end-1" />
+	<div
+		class="flex flex-col w-full h-full max-w-xl5 lg:col-start-2 lg:col-end-6"
+	>
 		<header class="bg-primary-dark border-y-black text-xl p-2">
 			Movement Pattern Based Workout Planner
 		</header>
 		<nav class="bg-primary-light flex flex-row">
 			<a class="p-2 hover:bg-primary-dark" href="/login">Login</a>
 			<a class="p-2 hover:bg-primary-dark" href="/admin">Admin</a>
+			{#if $userStore}
+				<h2 class="ml-auto my-auto justify-items-center content-center">
+					Logged in as: {$userStore.email}
+				</h2>
+			{/if}
 		</nav>
-		{#if $userStore}
-			<h2>{$userStore.email}</h2>
-		{/if}
-		<slot />
-	</main>
+		<main class="p-2 w-full">
+			<slot />
+		</main>
+	</div>
+	<aside class="hidden lg:block col-start-6 col-end-7" />
 </div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+      resolve: {
+				alias: {
+				$lib: path.resolve('./src/lib'),
+				'$lib/*': path.resolve('./src/lib/*'),
+				$styles: path.resolve('./src/lib/styles'),
+				'$styles/*': path.resolve('./src/lib/styles/*'),
+				$components: path.resolve('./src/lib/components'),
+				'$components/*': path.resolve('./src/lib/components/*')
+				}
+			}
+
 };
 
 export default config;

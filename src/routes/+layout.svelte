23140c1c5a78dc305a243e279c7b98f1c08ec63e
Joshua W Julian <joshuawjulian@gmail.com>
2022-08-30 08:22:34 -0400

    merge conflicts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,60 +20,9 @@
 				</h2>
 			{/if}
 		</nav>
-<<<<<<< HEAD
 		<main class="p-2 w-full">
 			<slot />
 		</main>
 	</div>
 	<aside class="hidden lg:block col-start-6 col-end-7" />
 </div>
-=======
-		{#if $userStore}
-			<h2>{$userStore.email}</h2>
-		{/if}
-		<slot />
-	</main>
-</wrapper>
-
-<style>
-	wrapper {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-	}
-	header {
-		width: 100%;
-		background-color: #a0a0a0;
-		font-size: 3rem;
-		border-bottom: 2px solid black;
-	}
-	nav {
-		border-bottom: 2px solid black;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-		height: 3rem;
-	}
-
-	nav a {
-		height: 100%;
-		min-width: 5rem;
-		padding: 1rem;
-		display: flex;
-  	justify-content: center;
-  	align-items: center;
-		background-color: #fff;
-	}
-
-	nav a:hover {
-		background-color:beige;
-	}
-	main {
-		width: 90%;
-		display: flex;
-		flex-direction: column;
-		background-color: #c0c0c0;
-		box-shadow: 1rem 1rem black;
-	}
-</style>
->>>>>>> d9e72f96d6d3a32119eede48017907bed90cba53

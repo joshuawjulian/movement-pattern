225aedbdb0373666b0b2dba8b845410a8b561004
Joshua W Julian <joshuawjulian@gmail.com>
2022-08-30 08:58:51 -0400

    Admin Changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
-	import { userStore } from '$lib/supabase';
+	import { supabase, userStore } from '$lib/supabase';
 	import '../app.css';
+
+	const logout = async () => {
+		await supabase.auth.signOut();
+	};
 </script>
 
-<div class="w-full h-full grid grid-cols-1 lg:grid-cols-6">
-	<aside class="hidden lg:block col-start-1 col-end-1" />
-	<div
-		class="flex flex-col w-full h-full max-w-xl5 lg:col-start-2 lg:col-end-6"
-	>
-		<header class="bg-primary-dark border-y-black text-xl p-2">
+<div class="w-full h-full flex">
+	<div class="flex flex-col w-full h-full">
+		<header class="bg-primary-dark text-xl font-semibold p-2">
 			Movement Pattern Based Workout Planner
 		</header>
-		<nav class="bg-primary-light flex flex-row">
-			<a class="p-2 hover:bg-primary-dark" href="/login">Login</a>
+		<nav class="bg-primary flex flex-row">
+			{#if !$userStore}
+				<a class="p-2 hover:bg-primary-dark" href="/login">Login</a>
+			{:else}
+				<a
+					class="p-2 hover:bg-primary-dark"
+					href="/login"
+					on:click|preventDefault={logout}>Logout</a
+				>
+			{/if}
 			<a class="p-2 hover:bg-primary-dark" href="/admin">Admin</a>
 			{#if $userStore}
 				<h2 class="ml-auto my-auto justify-items-center content-center">
@@ -20,9 +29,8 @@
 				</h2>
 			{/if}
 		</nav>
-		<main class="p-2 w-full">
+		<main class="h-max w-full">
 			<slot />
 		</main>
 	</div>
-	<aside class="hidden lg:block col-start-6 col-end-7" />
 </div>
---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,16 @@
+<div class="w-full h-full">
+	<nav class="w-full h-full flex flex-row bg-primary-light">
+		<a href="/admin/pattern" class="p-2 hover:bg-primary-dark hover:text-white"
+			>Pattern</a
+		>
+		<a href="/admin/pattern" class="p-2 hover:bg-primary-dark hover:text-white"
+			>Pattern</a
+		>
+		<a href="/admin/pattern" class="p-2 hover:bg-primary-dark hover:text-white"
+			>Pattern</a
+		>
+	</nav>
+	<main class="w-full h-full">
+		<slot />
+	</main>
+</div>
